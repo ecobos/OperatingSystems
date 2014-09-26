8013edc010037e3e51df45c6ba07c999b154c08f
Edgar Cobos <ecobos@edcobos.com>
2014-09-26 16:20:43 -0700

    Functioning program

--- lab3A.c
+++ lab3A.c
@@ -2,14 +2,14 @@
 # include <stdlib.h> 
 # include <sys/types.h> 
 # include <unistd.h> 
-void main(int argc, char *argv[]) 
+int main(int argc, char *argv[]) 
 { 
 	if (argc > 1) { 
-		execlp(“/bin/cat”, “cat”, argv[1], (char *) NULL); 
-		perror (“exec failure”); 
+		execlp("/bin/cat", "cat", argv[1], (char *) NULL); 
+		perror ("exec failure"); 
 		exit(1); 
  	} 
  
-	printf(“\n Usage: %s text_file\n”, *argv); 
+	printf("\n Usage: %s text_file\n", *argv); 
 	exit(1) ; 
 } 
